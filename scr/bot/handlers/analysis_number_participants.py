4f41200d7e5c82420b66895e622dd79a2f687dea
PyRusAdmin <zh.vitaliy92@yandex.ru>
2026-01-15 15:00:35 +0300

    Рефакторинг кода

--- scr/bot/handlers/analysis_number_participants.py
+++ scr/bot/handlers/analysis_number_participants.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
-
 from aiogram import types
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters import Command
@@ -29,17 +27,12 @@ async def getCountMembers(message: types.Message):
             try:
                 chat = await bot.get_chat(chat_id=actual_chat_id)
                 count = await bot.get_chat_member_count(chat_id=actual_chat_id)
-                now = datetime.now().strftime("%d.%m.%Y")
                 await message.answer(
                     f"📌 Название: {chat.title}\n"
                     f"👥 Количество участников: {count}\n"
                 )
             except TelegramBadRequest:
-                now = datetime.now().strftime("%d.%m.%Y")
-                # display_name = f"@{username}" if username else title
-
                 display_name = get_chat_link_by_chat_id(chat_id, user_id)
-
                 await message.answer(
                     f"⚠️ Бот не состоит в {display_name}\n"
                     f"ID: {chat_id}\n"
