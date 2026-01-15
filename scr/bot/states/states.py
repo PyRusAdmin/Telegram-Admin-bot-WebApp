62e50f47b9491fcb32bcfd9096fa3ee82dc537ff
PyRusAdmin <zh.vitaliy92@yandex.ru>
2026-01-15 11:48:09 +0300

    Рефакторинг кода

--- scr/bot/states/states.py
+++ scr/bot/states/states.py
@@ -4,7 +4,3 @@ from aiogram.fsm.state import StatesGroup, State
 
 class AnalysisState(StatesGroup):
     link_post = State()
-
-
-class ChooseWinnerState(StatesGroup):
-    waiting_for_link = State()
